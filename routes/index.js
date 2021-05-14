2d53cf3205d481f8c704a661bdd0367e9845e496
sehui0709 <84141913+sehui0709@users.noreply.github.com>
2021-05-15 02:48:57 +0900

    upload: login view

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/test', function(req, res, next) {
+  res.render('index2', { title: 'Express', name: 'han sh' });
+});
+
+
 module.exports = router;
