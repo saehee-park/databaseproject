0b2271c0b0fe642a1d9dfac2dea51544df94d8a9
JiWon <jiohning00@hanmail.net>
2021-05-19 15:00:23 +0900

    프로젝트 생성 페이지 render 시 고객 목록 전달, 프로젝트 생성 시 프로젝트, 고객 처리 추가

--- routes/projects.js
+++ routes/projects.js
@@ -33,22 +33,39 @@ router.post('/detail', catchErrors(async (req, res, next) => {
   res.render('project/detail', { projects: projects });
 }));
 
-router.get('/new', function (req, res, next) {
-  //customer list를 res로 전달
-  res.render('project/new', {});
-});
+router.get('/new', catchErrors(async (req, res, next) => {
+  const customers = await Customer.findAll();
+  res.render('project/new', { customers: customers });
+}));
 
 router.post('/new', catchErrors(async (req, res, next) => {
-  //새로 추가한 customer이면 customer create하고 id 받아서 적용
-  //아니면 그냥 바로 때리면 됨.
+  //새로운 프로젝트를 생성하면서 동시에 participation 생성해야함.
+
   console.log(req.body);
-  const proejct = await Project.create({
-    project_name: req.body.project_name,
-    start_date: req.body.start,
-    end_date: req.body.end,
-    state: req.body.state,
-    customer_id: req.body.customer_id
-  });
+
+  if(req.body.customer_id == null){
+    const customer = await Customer.create({
+      customer_name: req.body.customer_name
+    });
+
+    const proejct = await Project.create({
+      project_name: req.body.project_name,
+      start_date: req.body.start,
+      end_date: req.body.end,
+      state: req.body.state,
+      customer_id: customer.customer_id
+    });
+  }
+  else {
+    const proejct = await Project.create({
+      project_name: req.body.project_name,
+      start_date: req.body.start,
+      end_date: req.body.end,
+      state: req.body.state,
+      customer_id: req.body.customer_id
+    });
+  }
+  
 
   req.flash('success', 'Successfully Registered.');
   res.render('project/list', {});
